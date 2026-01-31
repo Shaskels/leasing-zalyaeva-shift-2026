4f1a148a41fa109474f8cb6341e938721d402919
Shaskels <allasidner028@gmail.com>
2026-01-31 15:43:56 +0700

    feat filters on car list

--- feature/carList/src/main/java/com/example/feature/carList/data/CarRepositoryImpl.kt
+++ feature/carList/src/main/java/com/example/feature/carList/data/CarRepositoryImpl.kt
@@ -9,6 +9,7 @@ import com.example.shared.car.data.CarService
 import com.example.shared.car.data.model.CarResponse
 import com.example.shared.car.data.toDomainCar
 import com.example.shared.car.domain.entity.Car
+import com.example.shared.filter.Filter
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -19,12 +20,13 @@ class CarRepositoryImpl @Inject constructor(private val carService: CarService):
         private const val PAGE_SIZE = 10
     }
 
-    override suspend fun getCars(query: String): Flow<PagingData<Car>> {
+    override fun getCars(query: String, filter: Filter): Flow<PagingData<Car>> {
         val pager: Pager<Int, CarResponse> = Pager(
             config = PagingConfig(pageSize = PAGE_SIZE, enablePlaceholders = false),
             pagingSourceFactory = {
                 CarPagingSource(
                     api = carService,
+                    filter = filter,
                     query = query,
                 )
             }
--- feature/carList/src/main/java/com/example/feature/carList/domain/GetCarsUseCase.kt
+++ feature/carList/src/main/java/com/example/feature/carList/domain/GetCarsUseCase.kt
@@ -3,8 +3,13 @@ package com.example.feature.carList.domain
 import androidx.paging.PagingData
 import com.example.feature.carList.domain.repository.CarRepository
 import com.example.shared.car.domain.entity.Car
+import com.example.shared.filter.Filter
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.Flow
 
-class GetCarsUseCase @Inject constructor(private val carRepository: CarRepository) :
-    suspend (String) -> Flow<PagingData<Car>> by carRepository::getCars
+class GetCarsUseCase @Inject constructor(private val carRepository: CarRepository) {
+
+    operator fun invoke(query: String, filter: Filter): Flow<PagingData<Car>> {
+        return carRepository.getCars(query, filter)
+    }
+}
